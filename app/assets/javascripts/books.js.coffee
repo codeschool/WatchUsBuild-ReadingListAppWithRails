d35fbb19a60029acaefd82c2b844cd01475b2d4d
Carlos Souza <carloshrsouza@gmail.com>
2014-05-15 18:19:17 -0400

    Add new book form.
    Show checkbox for genres.
    slideUp upon form submission.

--- app/assets/javascripts/books.js.coffee
+++ app/assets/javascripts/books.js.coffee
@@ -1,3 +1,7 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+$ ->
+  $('#show-form').on 'click', (e) ->
+    e.preventDefault()
+    $('.book-form').toggle()
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,6 +32,7 @@ class BooksController < ApplicationController
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render :show, status: :created, location: @book }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -71,6 +72,8 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :description, :amazon_id)
+      params.require(:book).permit(:title, :author,
+                                   :description, :amazon_id,
+                                   { genre_ids: []}, :rating)
     end
 end
