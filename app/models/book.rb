ea984f541fd3e398be5b8b126c132870b102ea1f
Carlos Souza <carloshrsouza@gmail.com>
2014-05-15 15:02:35 -0400

    Add finished and recent to Book.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ActiveRecord::Base
+
+  scope :finished, ->{ where.not(finished_on: nil) }
+  scope :recent, ->{ where('finished_on > ?', 2.days.ago) }
+
   def finished?
     finished_on.present?
   end
